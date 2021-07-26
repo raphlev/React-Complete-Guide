892d28e0487fd4d17f348e07b2c971068c6ac27c
raphlev <raphleveque@hotmail.fr>
2021-07-26 16:06:09 +0200

    u

--- 26-testing/src/components/Async.test.js
+++ 26-testing/src/components/Async.test.js
@@ -3,10 +3,13 @@ import Async from './Async';
 
 describe('Async component', () => {
   test('renders posts if request succeeds', async () => {
+    // Optional - we add these 4 lines of code below to add a mock on fetch API in order to avoid executing regular API from component that is tested here
     window.fetch = jest.fn();
     window.fetch.mockResolvedValueOnce({
       json: async () => [{ id: 'p1', title: 'First post' }],
     });
+    // End Mock
+
     render(<Async />);
 
     const listItemElements = await screen.findAllByRole('listitem');
